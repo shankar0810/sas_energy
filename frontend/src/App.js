454f56b7494f65f895a37b1c9d169bc0c5d86fc0
shankar0810 <shankarmerugu14@gmail.com>
2025-01-23 00:38:14 +0530

    frontend updated

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,12 @@
-import './App.css';
+import Header from "./components/header/Header";
+
+import "preline/preline";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <h1>Welcome to React</h1>
     </div>
   );
 }
